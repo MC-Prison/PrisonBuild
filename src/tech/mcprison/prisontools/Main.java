9b2eccbe8139c7054d3773ec874049d14ac420f0
DMP9Labs <dylanperks23@gmail.com>
2017-01-17 16:23:59 +0000

    Use java

---
+++ src/tech/mcprison/prisontools/Main.java
@@ -0,0 +1,17 @@
+package tech.mcprison.prisontools;/**
+ * Created by DMP9 on 17/01/2017.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override public void start(Stage primaryStage) {
+
+    }
+}
